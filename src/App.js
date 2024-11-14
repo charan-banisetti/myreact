49e6e78a7999aa36ed08b494669d056d4559b39b
chandakaBhaskar <bhaskarchandaka12345@gmail.com>
2024-11-14 16:40:44 +0530

    new

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Footer from './Footer';
 import LoginPage from './pages/LoginPage';
-import SignUpPage from './pages/SignUpPage'; // Import SignUpPage
+import SignUpPage from './pages/SignUpPage'; 
 
 import './App.css';
 
@@ -27,8 +27,8 @@ function App() {
             <Routes>
               <Route path="/" element={<HomePage addToCart={addToCart} />} />
               <Route path="/cart" element={<CartPage cartItems={cartItems} />} />
-              <Route path="/login" element={<LoginPage />} /> {/* Route for Login */}
-              <Route path="/signup" element={<SignUpPage />} /> {/* Route for Sign Up */}
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignUpPage />} />
             </Routes>
           </div>
         </div>
